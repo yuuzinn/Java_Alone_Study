0386419c74d5dadef35c2036c19e946cde0c2855
yeb0 <s110528@nate.com>
2023-04-15 22:36:37 +0900

    Docs : 04.15

--- src/JavaOfGod/version2/ch23/Practice04.java
+++ src/JavaOfGod/version2/ch23/Practice04.java
@@ -6,11 +6,22 @@ import java.util.Set;
 
 public class Practice04 {
 
+    final static String DIR = "C:\\Users\\mycom\\Desktop\\인텔리제이\\Java_Alone_Study\\file";
+
     public static void main(String[] args) {
         Practice04 test = new Practice04();
         for (int i = 0; i < 10; i++) {
             test.getSixNumber();
         }
+
+        System.out.println("------");
+
+        System.out.println(System.getProperty("user.home"));
+        System.out.println(System.getProperty("user.name"));
+        System.out.println(System.getProperty("user.dir") + "\\file");
+
+        System.out.println(System.getProperty(DIR));
+
     }
     public HashSet<Integer> getSixNumber() {
         Random rd = new Random();
---
+++ src/JavaOfGod/version2/ch25/ThreadSample.java
@@ -0,0 +1,7 @@
+package JavaOfGod.version2.ch25;
+
+public class ThreadSample extends Thread{
+    public void run() {
+        System.out.println("This is ThreadSample's run() method");
+    }
+}
